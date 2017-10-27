eae592e0a2b679649157ae19c86c04d9c9bd8148
tooltips <reddyapp.java@gmail.com>
2017-10-27 11:57:33 +0530

    springboot-demo web app files added

---
+++ src/main/java/com/reddy/springboot/web/SpringBootFirstWebApplication.java
@@ -0,0 +1,14 @@
+package com.reddy.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.reddy.springboot.web")
+public class SpringBootFirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebApplication.class, args);
+	}
+}
